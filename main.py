693cb4b34ccd93fff4670e2313b9e6bcccb0e126
Steph St <steph.streete@outlook.com>
2020-09-28 23:18:27 +0000

    finished the code

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 print("Steph's Calculator")
 num_1 = int(input("Enter your first number "))
-num_2 = int(input"Enter your second number")
+num_2 = int(input("Enter your second number"))
+
+sum_num = num_1 + num_2
+print("The answer of both numbers added together is " + str(sum_num))
+sum_num = num_1 - num_2
+print("The answer of the first number taken away from the second is " + str(sum_num))
+
 
